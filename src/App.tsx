5412b92785bb28f7edd6589a69a47a38886f8da5
kikucyo <kikucyo@gmail.com>
2023-04-29 02:27:20 +0000

    reactflow初期セットアップ

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import { ReactFlow } from "reactflow";
+import "reactflow/dist/style.css";
+const initialNodes = [
+  { id: "1", position: { x: 100, y: 50 }, data: { label: "1" } },
+  { id: "2", position: { x: 150, y: 200 }, data: { label: "2" } },
+];
+const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <ReactFlow nodes={initialNodes} edges={initialEdges} />
     </div>
   );
 }
